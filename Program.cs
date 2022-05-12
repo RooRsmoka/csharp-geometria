72c2a44b68bc052a89e946cb3a81cd1a55ccb1d5
RooRsmoka <chri.disa96@gmail.com>
2022-05-12 18:46:02 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace csharp_geometria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iValoreBase;
+            int iValoreAltezza;
+
+            Console.WriteLine("Inserisci un valore per la base del rettangolo");
+            if (!int.TryParse(Console.ReadLine(), out iValoreBase))
+            {
+                Console.WriteLine("Non hai inserito nessun valore per la base");
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Inserisci un valore per l'altezza del rettangolo");
+            if (!int.TryParse(Console.ReadLine(), out iValoreAltezza))
+            {
+                Console.WriteLine("Non hai inserito nessun valore per l'altezza");
+                Environment.Exit(1);
+            }
+
+            Console.WriteLine("Inserisci un nome da dare al rettangolo");
+            string sValoreNome = Console.ReadLine();
+
+            Rettangolo retOne = new Rettangolo(iValoreBase, iValoreAltezza, sValoreNome);
+            retOne.StampaRettangolo();
+        }
+    }
+}
---
+++ Rettangolo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_geometria
+{
+    internal class Rettangolo
+    {
+        private double dBaseRettangolo;
+        private double dAltezzaRettangolo;
+        private string sNomeRettangolo;
+
+        public Rettangolo(int baseRet, int altezzaRet, string sNomeRet)
+        {
+            dBaseRettangolo = baseRet;
+            dAltezzaRettangolo = altezzaRet;
+            sNomeRettangolo = sNomeRet;
+
+        }
+
+        public double CalcolaPerimetro()
+        {
+            return (dBaseRettangolo + dAltezzaRettangolo) * 2;
+        }
+
+        public double CalcolaArea()
+        {
+            return dBaseRettangolo * dAltezzaRettangolo;
+        }
+
+        public void StampaRettangolo()
+        {
+            Console.WriteLine("-- {0} --", sNomeRettangolo);
+            Console.WriteLine("Base: {0} cm", dBaseRettangolo);
+            Console.WriteLine("Altezza: {0} cm", dAltezzaRettangolo);
+            Console.WriteLine("Perimetro: {0} cm", CalcolaPerimetro());
+            Console.WriteLine("Area: {0} cm²", CalcolaArea());
+            Console.WriteLine();
+        }
+    }
+}
